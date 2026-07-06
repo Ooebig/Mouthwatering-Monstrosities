a4c77cb396a4c376dcff1fd16071b31d651dabf2
Arejay15 <richardcossv@gmail.com>
2026-07-05 22:39:18 -0400

    Implemented skill tree
    Needs a currency system to purchase upgrade, right now they are all free. no prefab yet

---
+++ Mouthwatering Monstrosities/Assets/Scripts/universal/SkillTree/AvailableNeighbor.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvailableNeighbor : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> myObjects = new List<GameObject>();
+    [SerializeField] private GameObject lines;
+
+    public void Start()
+    {
+        if (!gameObject.GetComponent<UpgradeInfo>().isPurchased && gameObject.GetComponent<UpgradeInfo>().upgradeType != UpgradeInfo.UpgradeType.NA)
+        {
+            gameObject.SetActive(false);
+        }
+        lines.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void Unlock()
+    {
+        for(int i = 0; i < myObjects.Count; i++)
+        {
+            if (myObjects[i] != null)
+            {
+                myObjects[i].SetActive(true);
+            }
+        }
+        lines.SetActive(true);
+    }
+}
